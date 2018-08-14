dc33b769272f787fbc79ecf2c2951decd1924c7e
Jae Yong Kim <kim900180@gmail.com>
2018-08-14 16:57:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  attr_accessor :lengthA, :lengthB, :lengthC
+
+  @@equilateral
+
+  def initialize(lengthA, lengthB, lengthC)
+    @lengthA = lengthA
+    @lengthB = lengthB
+    @lengthC = lengthC
+  end
+
+  def kind
+    if @lengthA == @lengthB && @lengthB == @lengthC
+      return :equilateral
+    elsif @lengthA == @lengthB && @lengthA != @lengthC
+      return :isosceles
+    elsif @lengthB == @lengthC && @lengthB != @lengthA
+      return :isosceles
+    elsif @lengthA == @lengthC && @lengthA != @lengthB
+      return :isosceles
+    elsif @lengthA != @lengthB && @lengthA != @lengthC
+      return :scalene
+    end
+  end
+
 end
